dc8ddbcaeae7b0a8c5048255bb1c7e974ae46404
João Victor Marques de Oliveira <victorgnr9@ufrn.edu.br>
2019-09-02 11:26:10 -0300

    Ajuste de cors para permitir requisicoes de localhost:4200

--- Startup.cs
+++ Startup.cs
@@ -48,7 +48,13 @@ namespace ReportsSystemAPI
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            app.UseCors(option => option.AllowAnyOrigin());
+            // app.UseCors(option => option.AllowAnyOrigin());
+            // app.UseCors(option => option.AllowAnyMethod());
+            // app.UseCors("AllowAnyOrigin");
+
+            app.UseCors(
+                options => options.WithOrigins("http://localhost:4200").AllowAnyMethod()
+            );
 
             if (env.IsDevelopment())
             {

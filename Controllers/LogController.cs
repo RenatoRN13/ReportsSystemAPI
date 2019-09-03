9e47321388bd01589d12e089d2078d58e116988b
Renato Filho <renatorncc@gmail.com>
2019-09-03 15:10:14 -0300

    Ajustando POST de logs e usuarios

--- Controllers/LogController.cs
+++ Controllers/LogController.cs
@@ -26,57 +26,25 @@ namespace ReportsSystemApi.Controllers
             return await _context.Logs.ToListAsync();
         }
 
-        // // GET api/Log/5
-        // [HttpGet("{id}")]
-        // public async Task<ActionResult<Log>> GetItem(int id)
-        // {
-        //     var item = await _context.Logs.FindAsync(id);
-
-        //     if(item == null){
-        //         return NotFound();
-        //     }
-
-        //     return item;
-        // }
-
         // POST api/Log
         [HttpPost]
         public async Task<ActionResult<Log>> Post(Log log)
         {
-            _context.Logs.Add(log);
-            await _context.SaveChangesAsync();
+            try{
+                Usuario usuario = await _context.Usuarios.FindAsync(log.idUsuario);
+                if(usuario != null){
+                    _context.Logs.Add(log);
+                    await _context.SaveChangesAsync();
 
-            return Ok("Sucesso!"); //CreatedAtAction(nameof(GetItem), new Log {id = log.id}, log);
+                    return Ok("Log registrado com sucesso!");
+                }
+                return Ok("Log não registrado. Usuário não informado!");             
+
+            } catch(Exception e){
+                Exception exception = new Exception(e.Message);
+                return Ok("Erro ao tentar registrar o log.");
+            }
+            
         }
-
-        // // PUT api/Log/5
-        // [HttpPut("{id}")]
-        // public async Task<IActionResult> Put(int id, Log log)
-        // {
-        //     if(id != log.id){
-        //         return BadRequest();
-        //     }
-
-        //     _context.Entry(log).State = EntityState.Modified;
-        //     await _context.SaveChangesAsync();
-
-        //     return NoContent();
-        // }
-
-        // // DELETE api/Log/5
-        // [HttpDelete("{id}")]
-        // public async Task<IActionResult> Delete(int id)
-        // {
-        //     var log = await _context.Logs.FindAsync(id);
-
-        //     if(log == null){
-        //         return NotFound();
-        //     }
-
-        //     _context.Logs.Remove(log);
-        //     await _context.SaveChangesAsync();
-
-        //     return NoContent();
-        // }
     }
 }
